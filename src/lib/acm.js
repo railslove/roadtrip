73077a5ab924a0b97bf1b659232f531945a75f5b
Timo Mämecke <timo@maemecke.com>
2019-06-12 00:51:40 +0200

    Rename siteName to domain in acm.js

--- src/lib/acm.js
+++ src/lib/acm.js
@@ -2,7 +2,7 @@ import AWS from 'aws-sdk'
 
 const acm = new AWS.ACM({ apiVersion: '2015-12-08' })
 
-export async function getCertificateForDomain(siteName) {
+export async function getCertificateForDomain(domain) {
   const data = await acm
     .listCertificates({
       CertificateStatuses: ['PENDING_VALIDATION', 'ISSUED']
@@ -10,7 +10,7 @@ export async function getCertificateForDomain(siteName) {
     .promise()
 
   // Get all certificates which match the domainName
-  const [tld, sld] = siteName.split('.').reverse()
+  const [tld, sld] = domain.split('.').reverse()
   const matchingCerts = data.CertificateSummaryList.filter(cert =>
     cert.DomainName.endsWith(`${sld}.${tld}`)
   )
@@ -23,21 +23,21 @@ export async function getCertificateForDomain(siteName) {
   )
 
   // Filter only the certificates which really work with the given site name.
-  // Either the certificate's names include the siteName directly, or it
+  // Either the certificate's names include the domain directly, or it
   // includes a wildcard. Wildcards only are valid for one level.
   // (e.g. *.baz.com doesn't work for foo.baz.baz.com)
   const certMatches = allCertData.filter(certData => {
-    // Strip the first path of the siteName domain in order to check against
+    // Strip the first path of the domain in order to check against
     // wildcard certificates. e.g. foo.bar.baz.io => bar.baz.io
-    const [_subdomain, ...otherSiteNamePaths] = siteName.split('.')
-    const siteNameForWildcardMatching = otherSiteNamePaths.join('.')
+    const [_subdomain, ...otherDomainParts] = domain.split('.')
+    const domainForWildcardMatching = otherDomainParts.join('.')
 
     for (let name of certData.Certificate.SubjectAlternativeNames) {
       // direct match
-      if (name === siteName) return true
+      if (name === domain) return true
 
       // wildcard match
-      if (name.replace('*.', '') === siteNameForWildcardMatching) return true
+      if (name.replace('*.', '') === domainForWildcardMatching) return true
 
       // no match
       return false
